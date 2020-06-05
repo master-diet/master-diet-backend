e5b5b9de3f345ae9f288a955345bc10aa3e56d97
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-06-05 18:52:01 +0200

    Small code refactor

--- src/main/java/pl/agh/edu/master_diet/exception/OAuth2AuthenticationProcessingException.java
+++ src/main/java/pl/agh/edu/master_diet/exception/OAuth2AuthenticationProcessingException.java
@@ -3,6 +3,7 @@ package pl.agh.edu.master_diet.exception;
 import org.springframework.security.core.AuthenticationException;
 
 public class OAuth2AuthenticationProcessingException extends AuthenticationException {
+
     public OAuth2AuthenticationProcessingException(String msg, Throwable t) {
         super(msg, t);
     }
