b5e1f1bb64f39fd30070481611fc890dbf5ab844
jfoder <jaroslaw.foder@gmail.com>
2020-06-07 19:44:48 +0200

    [MASTERDIET-39] Add Notification model

---
+++ src/main/java/pl/agh/edu/master_diet/core/model/database/Notification.java
@@ -0,0 +1,44 @@
+package pl.agh.edu.master_diet.core.model.database;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "notification")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private Boolean displayed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
