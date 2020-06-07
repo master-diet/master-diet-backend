0327a49e171bb9ca2f80831d0164d8c469556123
rafal <rafal.lisak1@gmail.com>
2020-06-07 16:51:56 +0200

    [MASTERDIET-12] Activity level and user plan response

---
+++ src/main/java/pl/agh/edu/master_diet/core/model/rest/userPlan/UserPlanResponse.java
@@ -0,0 +1,19 @@
+package pl.agh.edu.master_diet.core.model.rest.userPlan;
+
+import lombok.Builder;
+import lombok.NonNull;
+
+@Builder
+public class UserPlanResponse {
+
+    @NonNull
+    private Long userId;
+    @NonNull
+    private Integer calories;
+    @NonNull
+    private Float proteins;
+    @NonNull
+    private Float fat;
+    @NonNull
+    private Float carbohydrates;
+}
---
+++ src/main/java/pl/agh/edu/master_diet/core/model/shared/ActivityLevel.java
@@ -0,0 +1,11 @@
+package pl.agh.edu.master_diet.core.model.shared;
+
+public enum ActivityLevel {
+    BMR,
+    SEDENTARY,
+    LIGHT,
+    MODERATE,
+    ACTIVE,
+    VERY_ACTIVE
+
+}
