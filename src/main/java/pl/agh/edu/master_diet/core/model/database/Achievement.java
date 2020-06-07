9f507bfbddc018712e7832d3b41a73d2e626a04d
jfoder <jaroslaw.foder@gmail.com>
2020-06-07 20:04:16 +0200

    [MASTERDIET-39] Add UserAchievement and Achievement models

---
+++ src/main/java/pl/agh/edu/master_diet/core/model/database/Achievement.java
@@ -0,0 +1,43 @@
+package pl.agh.edu.master_diet.core.model.database;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "achievement")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Achievement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Integer points;
+
+    @Column(nullable = false)
+    private Integer completeCondition;
+
+    @Lob
+    private byte[] photo;
+}
