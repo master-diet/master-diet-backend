fd706c99b6d98a824375147c58ecb98a17bbd323
jfoder <jaroslaw.foder@gmail.com>
2020-07-26 16:46:28 +0200

    [MASTERDIET-13] Style fixes.

--- src/main/java/pl/agh/edu/master_diet/service/converter/ProductConverter.java
+++ src/main/java/pl/agh/edu/master_diet/service/converter/ProductConverter.java
@@ -8,12 +8,14 @@ import pl.agh.edu.master_diet.core.model.rest.product_browser.BaseProductInfo;
 public final class ProductConverter
         implements GenericConverter<Product, BaseProductInfo> {
 
+    private static final double GRAMS_PER_CALORIES_VALUE = 100.0;
+
     @Override
     public BaseProductInfo createFrom(final Product dto) {
         return BaseProductInfo.builder()
                 .id(dto.getId())
                 .name(dto.getName())
-                .calories((long) (dto.getCalories() / (100.0 / dto.getDefaultValue())))
+                .calories((long) (dto.getCalories() / (GRAMS_PER_CALORIES_VALUE / dto.getDefaultValue())))
                 .defaultValue(dto.getDefaultValue().longValue())
                 .unit(dto.getUnit())
                 .build();
--- src/main/java/pl/agh/edu/master_diet/service/converter/RecentProductConverter.java
+++ src/main/java/pl/agh/edu/master_diet/service/converter/RecentProductConverter.java
@@ -9,13 +9,15 @@ import pl.agh.edu.master_diet.core.model.rest.product_browser.BaseProductInfo;
 public final class RecentProductConverter
         implements GenericConverter<RecentProduct, BaseProductInfo> {
 
+    private static final double GRAMS_PER_CALORIES_VALUE = 100.0;
+
     @Override
     public BaseProductInfo createFrom(final RecentProduct dto) {
         Product product = dto.getProduct();
         return BaseProductInfo.builder()
                 .id(product.getId())
                 .name(product.getName())
-                .calories((long) (product.getCalories() / (100.0 / product.getDefaultValue())))
+                .calories((long) (product.getCalories() / (GRAMS_PER_CALORIES_VALUE / product.getDefaultValue())))
                 .defaultValue(product.getDefaultValue().longValue())
                 .unit(product.getUnit())
                 .build();
