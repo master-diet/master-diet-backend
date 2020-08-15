040ed7f89b576f49b091a8fb26867f521dcbccd1
jfoder <jaroslaw.foder@gmail.com>
2020-08-15 14:19:36 +0200

    [MASTERDIET-44] Imports cleanup.

--- src/main/java/pl/agh/edu/master_diet/core/model/database/Achievement.java
+++ src/main/java/pl/agh/edu/master_diet/core/model/database/Achievement.java
@@ -6,12 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "achievement")
