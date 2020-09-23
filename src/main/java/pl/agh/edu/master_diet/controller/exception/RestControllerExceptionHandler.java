90c579a041c9a1f8d83bd26f29e120be37e2689b
jplotnikowski <jakub.plotnikowski@grandparade.co.uk>
2020-09-23 12:08:14 +0200

    Update exception handler

--- src/main/java/pl/agh/edu/master_diet/controller/exception/RestControllerExceptionHandler.java
+++ src/main/java/pl/agh/edu/master_diet/controller/exception/RestControllerExceptionHandler.java
@@ -1,38 +1,32 @@
 package pl.agh.edu.master_diet.controller.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import pl.agh.edu.master_diet.core.model.standard.ErrorResponse;
-import pl.agh.edu.master_diet.core.model.standard.StandardApiResponse;
 import pl.agh.edu.master_diet.exception.DeleteException;
-
-import java.util.Arrays;
+import pl.agh.edu.master_diet.exception.UserPlanNotFoundException;
 
 @Slf4j
 @ControllerAdvice
 public final class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({DeleteException.class})
-    public ResponseEntity<StandardApiResponse> handleDeleteException(final DeleteException e) {
-        final StandardApiResponse apiResponse = StandardApiResponse.builder()
-                .success(false)
-                .message("Problem with deletion")
-                .error(ErrorResponse.builder()
-                        .code("deletion-error")
-                        .debugDetails(e.getLocalizedMessage())
-                        .build())
-                .build();
+    public ResponseEntity<Object> handleDeleteException(final DeleteException e,
+                                                        final WebRequest request) {
+        log.error(e.getMessage(), e);
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
 
-        log.warn(e.getMessage());
-        log.warn(Arrays.toString(e.getStackTrace()));
-
-        return ResponseEntity
-                .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body(apiResponse);
+    @ExceptionHandler({UserPlanNotFoundException.class})
+    public ResponseEntity<Object> handleUserPlanNotFoundException(final UserPlanNotFoundException e,
+                                                                  final WebRequest request) {
+        log.error(e.getMessage(), e);
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY, request);
     }
 }
 
