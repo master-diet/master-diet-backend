5f2c7f2201ee41b6ea8a29b8bbdf554293a40815
Jakub Płotnikowski <jakub77plotnikowski@gmail.com>
2020-10-09 20:07:14 +0200

    Update SpringFoxConfig

--- src/main/java/pl/agh/edu/master_diet/config/SpringFoxConfig.java
+++ src/main/java/pl/agh/edu/master_diet/config/SpringFoxConfig.java
@@ -2,8 +2,8 @@ package pl.agh.edu.master_diet.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.context.annotation.Import;
+import pl.agh.edu.master_diet.security.CurrentUser;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -19,6 +19,7 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(CurrentUser.class)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
